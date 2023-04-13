39ea52d9d7003eab59455a1fbd6d934c3f428c9d
Andre Sayej <andresayej@gmail.com>
2023-04-13 22:02:16 +0300

    Create 1Password Shell Plugin for Laravel Forge CLI

---
+++ plugins/laravelforge/forge.go
@@ -0,0 +1,24 @@
+package laravelforge
+
+import (
+	"github.com/1Password/shell-plugins/sdk"
+	"github.com/1Password/shell-plugins/sdk/needsauth"
+	"github.com/1Password/shell-plugins/sdk/schema"
+	"github.com/1Password/shell-plugins/sdk/schema/credname"
+)
+
+func LaravelForgeCLI() schema.Executable {
+	return schema.Executable{
+		Name:    "Laravel Forge CLI",
+		Runs:    []string{"forge"},
+		DocsURL: sdk.URL("https://forge.laravel.com/docs/1.0/cli.html"),
+		NeedsAuth: needsauth.IfAll(
+			needsauth.NotForHelpOrVersion(),
+		),
+		Uses: []schema.CredentialUsage{
+			{
+				Name: credname.APIToken,
+			},
+		},
+	}
+}
